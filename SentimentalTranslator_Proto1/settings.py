a78bcf6b71facec3f77f408f857e8b00a526125c
Ubuntu <theo@servtheo.rjr2h30kp30u3o3xw2yz4qxswe.ax.internal.cloudapp.net>
2017-04-05 13:31:49 +0000

    Handle errors better

--- SentimentalTranslator_Proto1/settings.py
+++ SentimentalTranslator_Proto1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ilqwv+wej#y=(x#%$sxaj&%5*j2052ne(9cr*!l4j7=xdqh#^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['translateme.westeurope.cloudapp.azure.com']
 
